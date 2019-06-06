5c707539be2b8e8a3a31ec835507b16d721ae70b
gioestevam <giovannibestevam@gmail.com>
2019-06-06 00:37:03 -0300

    'save-and-read-file-csv'

--- dataScience/views.py
+++ dataScience/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+import pandas as pda
 
 # Create your views here.
 def dataScienceHtml(request):
-    return render(request, 'importExcel.html', {})
+    if request.method == 'POST' and request.FILES['my_file']:
+        my_file = request.FILES['my_file']
+        fs = FileSystemStorage()
+        filename = fs.save(my_file.name, my_file)
+        fileUrl  = fs.url(filename)
+        csvObject = pda.read_csv(fileUrl)
+        print(csvObject.head(5))
+        return render(request, 'graphic.html', { 'csvObjectColumns': csvObject.columns })
+    return render(request, 'importExcel.html')
+    
